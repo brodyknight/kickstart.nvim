05da9db3f2a0b58e22c0251881f11eac78a487da
Brody Knight <git@brodyk.com>
2024-03-20 19:19:38 +0000

    Add terraformls

--- init.lua
+++ init.lua
@@ -540,6 +540,7 @@ require('lazy').setup {
         markdownlint = {},
         marksman = {},
         prettierd = {},
+        terraformls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
