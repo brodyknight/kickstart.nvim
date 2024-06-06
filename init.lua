7e6952fefe358dc0733b6b80e61711a9f36a6e57
Brody Knight <git@brodyk.com>
2024-06-06 11:50:41 +0100

    Add tf filetype to terraformls

--- init.lua
+++ init.lua
@@ -544,7 +544,11 @@ require('lazy').setup {
         markdownlint = {},
         marksman = {},
         prettierd = {},
-        terraformls = {},
+        terraformls = {
+          filetypes_include = {
+            "tf"
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
